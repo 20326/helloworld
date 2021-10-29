933ac7637c25983a7f9d4aa87641c09dff989527
brian <brian.upon@gmail.com>
2021-10-29 16:47:27 +0800

    Init

---
+++ merlin/config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "HelloWorld", 
+    "home_url": "Module_shadowsocks.asp", 
+    "md5": "f9b8c31f386367adc9467a5cc37ea9cf", 
+    "module": "helloword", 
+    "title": "HelloWorld", 
+    "version": "2.0.0"
+}
