1596b5e5560701b587938a83dea3c5564e390315
brian <brian.upon@gmail.com>
2021-11-21 00:27:29 +0800

    fixup

--- merlin/config.json.js
+++ merlin/config.json.js
@@ -3,7 +3,7 @@
     "archver": "arm64-v8a", 
     "description": "HelloWorld", 
     "home_url": "Module_helloworld.asp", 
-    "md5": "02d708d31813af5fd46510d6c9e21a5f", 
+    "md5": "908b101a69ba63a5e034299e74c7ef12", 
     "module": "helloworld", 
     "title": "HelloWorld", 
     "version": "2.0.0"
