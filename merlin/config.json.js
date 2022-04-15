aeccd343891f3fdac7d1661942a9aa1be5858cde
brian <brian.upon@gmail.com>
2022-04-15 18:01:55 +0800

    2.0.4

--- merlin/config.json.js
+++ merlin/config.json.js
@@ -3,8 +3,8 @@
     "archver": "arm64-v8a", 
     "description": "HelloWorld", 
     "home_url": "Module_helloworld.asp", 
-    "md5": "7a33896954b1f9130a0f7e6647f0786e", 
+    "md5": "18949585cb65bdd5e115a7459e2cbddc", 
     "module": "helloworld", 
     "title": "HelloWorld", 
-    "version": "2.0.1"
+    "version": "2.0.4"
 }
