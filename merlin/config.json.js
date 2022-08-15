5f59e4b3df393202269c5dd82d36711851f4a0a2
brian <brian.upon@gmail.com>
2022-08-15 12:37:05 +0800

    update: xray v1.5.9

--- merlin/config.json.js
+++ merlin/config.json.js
@@ -1,10 +1,10 @@
 {
-    "arch": "arm64",
-    "archver": "arm64-v8a",
-    "description": "HelloWorld",
-    "home_url": "Module_helloworld.asp",
-    "md5": "720b754722825b66ce3964dcf0e82135",
-    "module": "helloworld",
-    "title": "HelloWorld",
-    "version": "2.0.8"
+    "arch": "arm64", 
+    "archver": "arm64-v8a", 
+    "description": "HelloWorld", 
+    "home_url": "Module_helloworld.asp", 
+    "md5": "73c342fa04ef333c153d1f4d29031aec", 
+    "module": "helloworld", 
+    "title": "HelloWorld", 
+    "version": "2.0.9"
 }
